d96358923da5b0050bdd0d5c0686bdbd170d9f75
Efim Shchegolev <efimblashck@yandex.ru>
2022-12-12 18:12:32 +0300

    feat: сверстать компонент Portfolio

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,36 @@
+import './Portfolio.css'
+import arrowPath from '../../images/arrow-link.svg'
+
+function Portfolio() {
+  return (
+    <section className='portfolio'>
+      <div className='portfolio__container'>
+        <h2 className='portfolio__title'>Портфолио</h2>
+        <nav className='portfolio__navigation'>
+          <ul className='portfolio__nav-links'>
+            <li className='portfolio__list-item'>
+              <a href='#' className='portfolio__link'>
+                Статичный сайт
+                <img src={arrowPath} className='portfolio__arrow' />
+              </a>
+            </li>
+            <li className='portfolio__list-item'>
+              <a href='#' className='portfolio__link'>
+                Адаптивный сайт
+                <img src={arrowPath} className='portfolio__arrow' />
+              </a>
+            </li>
+            <li className='portfolio__list-item'>
+              <a href='#' className='portfolio__link'>
+                Одностраничное приложение
+                <img src={arrowPath} className='portfolio__arrow' />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+  )
+}
+
+export default Portfolio
