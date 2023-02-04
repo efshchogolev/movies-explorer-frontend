0f33d958ca1a8d4c9e35b497e4164c43c377e340
Efim Shchegolev <efimblashck@yandex.ru>
2023-02-04 21:53:39 +0300

    feat: добавить прелоадер

--- src/components/HeaderLanding/HeaderLanding.jsx
+++ src/components/HeaderLanding/HeaderLanding.jsx
@@ -1,7 +1,6 @@
 import './HeaderLanding.css'
 import logoPath from '../../images/logo.svg'
 import { Link } from "react-router-dom";
-import Navigation from '../Navigation/Navigation';
 
 function HeaderLanding() {
   return (
@@ -9,11 +8,10 @@ function HeaderLanding() {
       <Link to='/'>
         <img src={logoPath} alt="логотип" className='landing-header__logo' />
       </Link>
-      <Navigation />
-      {/* <div className='landing-header__auth-container'>
+      <div className='landing-header__auth-container'>
         <Link className='landing-header__link' to='/signup'>Регистрация</Link>
         <Link to='signin'><button className='landing-header__button'>Войти</button></Link>
-      </div> */}
+      </div>
     </header>
   )
 }
