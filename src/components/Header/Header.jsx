5e0adcd5cc6aaeb836de12dc907c437d0d55bc86
Efim Shchegolev <efimblashck@yandex.ru>
2023-02-05 22:32:21 +0300

    feat: сделать 768 версию фильмов

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,16 +5,18 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className='header'>
-      <div className='header__films-container'>
+      <div className='header__container'>
         <Link to='/' >
           <img src={logoPath} alt="логотип" className='header__logo' />
         </Link>
-        <Link className='header__link header__link_films_bold header__link_films' to='/movies'>Фильмы</Link>
-        <Link className='header__link header__link_films' to='/saved-movies'>Сохранённые фильмы</Link>
-      </div>
-      <div className='header__account-container'>
-        <Link className='header__link header__link_account' to='/profile'>Аккаунт</Link>
-        <Link to='/profile' className='header__link header__link_avatar'><div className='header__avatar-container'></div></Link>
+        <div className='header__films-container'>
+          <Link className='header__link header__link_films_bold header__link_films' to='/movies'>Фильмы</Link>
+          <Link className='header__link header__link_films' to='/saved-movies'>Сохранённые фильмы</Link>
+        </div>
+        <div className='header__account-container'>
+          <Link className='header__link header__link_account' to='/profile'>Аккаунт</Link>
+          <Link to='/profile' className='header__link header__link_avatar'><div className='header__avatar-container'></div></Link>
+        </div>
       </div>
     </header>
   )
