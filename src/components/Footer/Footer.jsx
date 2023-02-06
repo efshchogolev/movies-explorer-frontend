8c9b195a8dd6d7c12263cd2dc794caf44361f185
Efim Shchegolev <efimblashck@yandex.ru>
2023-02-06 15:15:09 +0300

    feat: сделать 320 версию лендинга

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import './Footer.css'
 function Footer() {
   return (
     <footer className='footer'>
-      <p className='footer__title'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <p className='footer__main-text'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
       <div className='footer__container'>
-        <p className='footer__text'>&copy; 2022</p>
+        <p className='footer__text'>&copy;2022</p>
         <ul className='footer__nav-links'>
           <li className='footer__link'>Яндекс.Практикум</li>
           <li className='footer__link'>Github</li>
