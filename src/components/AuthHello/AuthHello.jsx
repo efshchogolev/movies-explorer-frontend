549c05c0daf10e409bcefd3786cd65c35d9792c0
Efim Shchegolev <efimblashck@yandex.ru>
2023-02-07 03:51:30 +0300

    feat: сделать 320 версию логина/регистрации/404

--- src/components/AuthHello/AuthHello.jsx
+++ src/components/AuthHello/AuthHello.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function AuthHello({ helloText }) {
   return (
     <div className='auth-hello'>
-      <Link to='/'>
+      <Link to='/' className='auth-hello__link'>
         <img src={logoPath} alt="логотип" className='auth-hello__logo' />
       </Link>
       <p className='auth-hello__text'>{helloText}</p>
