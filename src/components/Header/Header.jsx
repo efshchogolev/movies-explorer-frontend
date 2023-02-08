dec60bccac604fd2594e5aaea16807131fcef91b
Efim Shchegolev <efimblashck@yandex.ru>
2023-02-08 21:57:58 +0300

    feat: сделать бургерное меню

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,11 @@ import './Header.css'
 import logoPath from '../../images/logo.svg'
 import { Link } from "react-router-dom";
 import Menu from '../Menu/Menu'
+import BurgerMenu from '../BurgerMenu/BurgerMenu';
 
-function Header({ children }) {
+
+function Header() {
+
   return (
     <header className='header'>
       <div className='header__container'>
@@ -20,6 +23,8 @@ function Header({ children }) {
             <Link to='/profile' className='menu__link menu__link_avatar'><div className='menu__avatar-container'></div></Link>
           </div>
         </Menu>
+
+        <BurgerMenu />
       </div>
     </header>
   )
